408cd386e6718cb7b38a77e16106500dddffb948
Priyam <priyam.poddar2@gmail.com>
2021-07-07 21:56:46 +0530

    ready to deploy

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import Navbar from './routes/Navbar';
 import Home from './routes/Home';
 import HandshakeContextProvider from './Context';
-import NotFound from './routes/NotFound';
 
 function App() {
   return (
@@ -14,11 +13,9 @@ function App() {
           < Navbar />
           <Switch>
             <Route exact path="/" > <Home /> </ Route>
-            <Route path = "/room" exact > < NotFound /> </Route>
             <HandshakeContextProvider>
               <Route path="/room/:roomID" exact  component = {Room2} />
             </HandshakeContextProvider>
-            <Route path = "*" > < NotFound /> </Route>
           </Switch>
         </BrowserRouter>
     </div>
