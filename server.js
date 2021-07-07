3b4e47d60e7d5bb908bc727ba29b8af9d27ae9de
Priyam <priyam.poddar2@gmail.com>
2021-07-07 22:12:21 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -71,12 +71,13 @@ io.on('connection', socket=>{
     });
 });
 
-if(process.env.PROD){
-    app.use(express.static(path.join(__dirname, './client/public')));
-    app.get('*', (req,res)=>{
-        res.sendFile(path.join(__dirname, './client/public/index.html'));
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+  }
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
